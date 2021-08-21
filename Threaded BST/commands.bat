f41c8a32742308054f4f57ee6dbf776b8b4bdc27
prateekshyap <prateekshyapriyadarshini@gmail.com>
2021-08-21 22:05:08 +0530

    Reference

---
+++ Threaded BST/commands.bat
@@ -0,0 +1,10 @@
+dot -Tpng bstStructure0.gv -o struct0.png
+dot -Tpng bstStructure1.gv -o struct1.png
+dot -Tpng bstStructure2.gv -o struct2.png
+dot -Tpng bstStructure3.gv -o struct3.png
+dot -Tpng bstStructure4.gv -o struct4.png
+dot -Tpng bstStructure5.gv -o struct5.png
+dot -Tpng bstStructure6.gv -o struct6.png
+dot -Tpng bstStructure7.gv -o struct7.png
+dot -Tpng bstStructure8.gv -o struct8.png
+dot -Tpng bstStructure9.gv -o struct9.png
